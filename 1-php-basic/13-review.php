b4bf6f712196f63d662da3e9cb3d0d79143c3993
ndnhan132 <ndnhan132@gmail.com>
2018-05-17 20:04:14 +0700

    commit project

---
+++ 1-php-basic/13-review.php
@@ -0,0 +1,55 @@
+<?php
+echo "<a href=\"./\">BACK</a>";
+
+echo "<h1>13. REVIEW</h1>";
+/* EXERCISE :
+1. Create an Array about data of authors like :
+$authors = [
+[
+"name" => "Lorem",
+"age" => "18",
+"posts_quantity" => "5",
+],
+[
+"name" => "Lorem",
+"age" => "18",
+"post_quantity" => "5",
+],
+];
+
++ At least 5 authors.
+
+2. Display it into website depends on Bootstrap 4 Table, in 4 column : Name, Age, Post Counting, Salary.
+
+3. Make sure, if posts_quantity > 10, show at Salary Column = posts_quantity * 10000, otherwise, display 0.
+
+ */
+echo "<br><br>DO EXERCISE INSIDE COMMENT CODE BELOW THIS LINE<hr>";
+
+$authors= [
+    [
+        "name"=>"aaa",
+        "age"=>"23",
+        "post-quantity"="15"
+    ]
+    [
+        "name"=>"ccc",
+        "age"=>"23",
+        "post-quantity"="15"
+    ]
+    [
+        "name"=>"bbb",
+        "age"=>"23",
+        "post-quantity"="15"
+    ]
+    [
+        "name"=>"eee",
+        "age"=>"23",
+        "post-quantity"="15"
+    ]
+    [
+        "name"=>"rrr",
+        "age"=>"23",
+        "post-quantity"="15"
+    ]
+]
